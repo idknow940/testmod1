07f94697b78c75d2b1f8e675d7a452220a8f9305
idknow94 <i.dont.kei.94@gmail.com>
2021-12-23 19:00:54 +0400

    added parchment mappings

---
+++ src/main/java/com/maindotpy/testmod/block/ModBlocks.java
@@ -0,0 +1,37 @@
+package com.maindotpy.testmod.block;
+
+import com.maindotpy.testmod.TestMod;
+import com.maindotpy.testmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Block> SOUL_BLOCK = registerBlock("soul_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(2f)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
